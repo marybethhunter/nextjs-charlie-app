17055a37d50866d7a4e491357d1936ed2af7482e
Mary Beth Hunter <86667443+marybethhunter@users.noreply.github.com>
2023-08-02 18:15:04 -0500

    test

--- pages/api/actions.js
+++ pages/api/actions.js
@@ -7,7 +7,7 @@ export default withApiAuthRequired(async function actions(req, res) {
 
     });
     
-    const devEnvironment = 'development';
+    const devEnvironment = 'production';
     const apiPort = process.env.API_PORT || 3001;
     const path =
       devEnvironment === 'development'
